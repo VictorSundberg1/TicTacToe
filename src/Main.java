458df7274e1e6cb31bccbc1d9aa357d9746f305a
VictorSundberg1 <victor.sundberg1@hotmail.com>
2024-10-08 12:43:17 +0200

    Updated main with new menu for playing again or exiting

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Welcome to TicTacToe");
@@ -11,33 +10,33 @@ public class Main {
 
         System.out.println("Enter name of player 2 (O)");
         Player player2 = new Player(sc.nextLine(), 'O');
-
         TicTacToe game = new TicTacToe(player1, player2);
 
-        game.startGame();
-        game.resetGameBoard();
+        do {
+            game.startGame();
+            game.resetGameBoard();
+        }while (showMenu(player1, player2));
 
-        boolean running = true;
-
-        while(running){
-            System.out.println(player1.getName() + " has " + player1.getWins() + " Wins!");
-            System.out.println(player2.getName() + " has " + player2.getWins() + " Wins!");
-            System.out.println("Enter '1' to play again!");
-            System.out.println("Enter '2' to exit");
-
-
-            String input = sc.nextLine();
-            switch (input) {
-                case "1":
-                    game.startGame();
-                    game.resetGameBoard();
-                    break;
-                case "2":
-                    System.out.println("Thank you for playing");
-                    running = false;
-                    break;
-            }
-        }
+        sc.close();
     }
 
+    private static boolean showMenu(Player player1, Player player2){
+        Scanner sc = new Scanner(System.in);
+        System.out.println(player1.getName() + " has " + player1.getWins() + " Wins!");
+        System.out.println(player2.getName() + " has " + player2.getWins() + " Wins!");
+        System.out.println("Enter '1' to play again!");
+        System.out.println("Enter '2' to exit");
+
+        String input = sc.nextLine();
+        switch (input) {
+            case "1":
+                return true;
+            case "2":
+                return false;
+            default:
+                System.out.println("Invalid input!");
+                return showMenu(player1, player2);
+        }
+    }
 }
+
